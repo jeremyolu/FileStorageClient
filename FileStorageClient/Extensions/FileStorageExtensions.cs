794c2777b4895bf13a00a109ce6bfce1adb17a3a
Jeremy Olu <jeremy-olu@hotmail.com>
2025-01-01 14:50:49 +0000

    Create FileStorageExtensions.cs

---
+++ FileStorageClient/Extensions/FileStorageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FileClient.Interfaces;
+using FileClient.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileStorageClient.Extensions
+{
+    public static class FileStorageExtensions
+    {
+        public static IServiceCollection AddStorageFileClient(this IServiceCollection services, string connectionString)
+        {
+            return services.AddSingleton<IStorageClient>(client => new StorageClient(connectionString));
+        }
+    }
+}
